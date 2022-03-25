f74168cb10ae13da0ac0d39cfd114bab937fc8ce
Ulisses Silverio <ulissessuporteti01@gmail.com>
2022-03-25 18:31:08 -0300

    firebase first configuration

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// configuração dos meus web app´s firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDtyzLFOkzzRfT03sWed0f2c0gCRicUjz8",
+  authDomain: "htlm-and-firebase.firebaseapp.com",
+  databaseURL: "https://htlm-and-firebase-default-rtdb.firebaseio.com",
+  projectId: "htlm-and-firebase",
+  storageBucket: "htlm-and-firebase.appspot.com",
+  messagingSenderId: "436127163299",
+  appId: "1:436127163299:web:b325b2f946ec1e7cbd3016"
+};
 
-
-
+//iniciando o servidor Firebase
+firebase.initializeApp(firebaseConfig);
